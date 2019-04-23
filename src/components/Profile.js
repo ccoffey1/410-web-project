a2e8a4b060eb52831facce3611f88b21f4b8f0fc
Tellyou6bu6 <yunbao@buffalo.edu>
2019-04-23 17:26:18 -0400

    added module2

--- src/components/Profile.js
+++ src/components/Profile.js
@@ -97,6 +97,7 @@ export default class Profile extends Component {
                   <Typography variant="display1" style={grayStyle}> Modules Completed </Typography>
                   <Typography variant="subtitle1" style={{ marginLeft: 20, marginTop: 0 }}> Click the module to replay </Typography>
                   <Link to="/Module1"><Typography variant="h5" style={{margin: 20}}> Module 1 - Variables </Typography></Link>
+                  <Link to="/Module2"><Typography variant="h5" style={{margin: 20}}> Module 2 - If statement </Typography></Link>
             </Paper>
           </Grid>
         </Grid>
--- src/index.js
+++ src/index.js
@@ -36,6 +36,12 @@ ReactDOM.render(
             objectives="Now, let's try to assign a variable name. Let's call the string variable var1 and assign the value of cat to it. Then let's call the int variable var2 and assign the number 3 to it."
             hints={["Consider following examples:", <li>float num = 0.12;</li>, <li>bool dog = true;</li>,<li>Don't forget the semicolon at the end.</li>]}
             codeBlock={["//Master JAVA Variables! \n\npublic static void main(String args[]){\n\n//Create a string variable 'cat'\nstring ",<input type='text' id='bb'placeholder="e.g. var1 = 'cat';"/>,"= 'cat';", "\n\n//Create an int variable\nint ", <input type='text' id='aa'placeholder="e.g. var2 = 3;"/>, "= 3;", "\n}"]}/>} />
+          <Route path="/Module2" render={(routeProps) => <Module {...routeProps} title="Basic Java: If statement"
+            category="Welcome!"
+            description={"WOW! You are getting it so fast. Now, let's dig into conditional statements. The first conditional statement is if statement, you will use if statement a lot on the way to become a professional programmer. Java supports the usual logical conditions, you can use these conditions to perform different actions for different dicisions."}
+            objectives="Fill out the text boxes, If a is less than b, then print out 'Hello World'."
+            hints={["Consider following hints:", <li>Use if to specify a block of code to be executed, if a specified condition is true.</li>, <li>Make sure your if is in lowercase letter.</li>]}
+            codeBlock={["//Master JAVA If statement! \n\npublic static void main(String args[]) {\n\n//Create an if statement \nint a = 3; \nint b = 5;\n\n", <input type='text' id='bb'placeholder="If notation"/>, "(", <input type = 'text' id='if' placeholder='a is less then b'/>,")","{\n\tSystem.out.print('Hello World');\n}", "\n}"]}></Module>}/>
           <Route path="/Profile" component={Profile} />
         </div>
     </Router>,
