dd401acc7eab4f104b8a2677f116e4086a08673f
ubbh <bohanghu@buffalo.edu>
2019-04-22 22:48:56 -0400

    Code on Variable module
    Add codes for Variable module.

--- src/App.js
+++ src/App.js
@@ -24,7 +24,12 @@ class App extends Component {
           <Module 
             category="Welcome!"
             hints={["Lorem Ipsum is simply dummy text of the printing and typesetting industry.", <li>It is a long established fact that a reader will be distracted by the readable content of a page when looking at its layout.</li>, <li>Item 2</li>]}
-            codeBlock={"public class Testing {\n\n}"}>
+            codeBlock={["//Mater the Variables! \n\npublic class Testing {\n\n//Create a string variable 'cat'\nstring ",<input type='text' id='bb'placeholder="e.g. var1 = 'cat';"/>, "\n\n//Create an int variable\nint ", <input type='text' id='aa'placeholder="e.g. var2 = 3;"/>, "\n\n}"]}>            
+          </Module>
+          <Module 
+            category="Welcome!"
+            hints={["Lorem Ipsum is simply dummy text of the printing and typesetting industry.", <li>It is a long established fact that a reader will be distracted by the readable content of a page when looking at its layout.</li>, <li>Item 2</li>]}
+            codeBlock={["//Mater the Variables! \n\npublic class Testing {\n\n//Create a string variable 'cat'\nstring ",<input type='text' id='bb'placeholder="e.g. var1 = 'cat';"/>, "\n\n//Create an int variable\nint ", <input type='text' id='aa'placeholder="e.g. var2 = 3;"/>, "\n\n}"]}>            
           </Module>
         </MuiThemeProvider>
       </div>
