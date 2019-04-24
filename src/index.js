6a8115623ff84cbd4ab6b408e3a7cf3d0f0c9595
Eithne Amos <emawrites77@gmail.com>
2019-04-24 19:59:11 -0400

    Updated description and objective for Module 1

--- src/index.js
+++ src/index.js
@@ -32,8 +32,8 @@ ReactDOM.render(
           <Route exact path="/" component={App} />
           <Route path="/Module1" render={(routeProps) => <Module {...routeProps} title="Basic Java: Variables"
             category="Welcome!"
-            description={"Welcome to Undici's Java coding game. These will walk you through Java development step by step. First, you'll start by assigning names to the variables.You can edit code in the text box. Do you see the code in the middle? That's how you declare a variable. String and int are variable types."}
-            objectives="Now, let's try to assign a variable name. Let's call the string variable var1 and assign the value of cat to it. Then let's call the int variable var2 and assign the number 3 to it."
+            description={"First, we're going to learn about variables. Variables in code, like in math, can take on any value that you assign to them. However, unlike variables in math, Java variables need to be assigned a type. The type of the variable determines the value it can hold. So, for example, the type 'String' can hold text data, and the type 'int' can hold integer values. There are more types in Java, but we'll start by looking at an example of type String."}
+            objectives="Now, let's try this out by assigning a value to a variable. In the code on the next panel, there is a variable named 'ballColor.' Assign 'ballColor' the value 'orange.'"
             hints={["Consider following examples:", <li>float num = 0.12;</li>, <li>bool dog = true;</li>,<li>Don't forget the semicolon at the end.</li>]}
             codeBlock={["//Master JAVA Variables! \n\npublic static void main(String args[]){\n\n//Create a string variable 'cat'\nstring ",<input type='text' id='bb'placeholder="e.g. var1 = 'cat';"/>,"= 'cat';", "\n\n//Create an int variable\nint ", <input type='text' id='aa'placeholder="e.g. var2 = 3;"/>, "= 3;", "\n}"]}/>} />
           <Route path="/Module2" render={(routeProps) => <Module {...routeProps} title="Basic Java: If statement"
