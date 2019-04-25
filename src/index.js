cf23740c941fe9c7236bf7dc3689bec8fd903d05
ubbh <bohanghu@buffalo.edu>
2019-04-25 15:34:36 -0400

    Update index.js

--- src/index.js
+++ src/index.js
@@ -35,7 +35,7 @@ ReactDOM.render(
             description={"Welcome to Undici's Java coding game. These will walk you through Java development step by step. First, you'll start by assigning names to the variables.You can edit code in the text box. Do you see the code in the middle? That's how you declare a variable. String and int are variable types."}
             objectives="Now, let's try to assign a variable name. Let's call the string variable var1 and assign the value of cat to it. Then let's call the int variable var2 and assign the number 3 to it."
             hints={["Consider following examples:", <li>float num = 0.12;</li>, <li>bool dog = true;</li>,<li>Don't forget the semicolon at the end.</li>]}
-            codeBlock={["//Master JAVA Variables! \n\npublic static void main(String args[]){\n\n//Create a string variable 'cat'\nstring ",<input type='text' id='bb'placeholder="e.g. var1 = 'cat';"/>,"= 'cat';", "\n\n//Create an int variable\nint ", <input type='text' id='aa'placeholder="e.g. var2 = 3;"/>, "= 3;", "\n}"]}/>} />
+            codeBlock={["//Master JAVA Variables! \n\npublic static void main(String args[]) {\n\nstring ballColor = ",<input type='text' id='ballcolor'placeholder="e.g. 'red'"/> ]}/>} />
           <Route path="/Module2" render={(routeProps) => <Module {...routeProps} title="Basic Java: If statement"
             category="Welcome!"
             description={"WOW! You are getting it so fast. Now, let's dig into conditional statements. The first conditional statement is if statement, you will use if statement a lot on the way to become a professional programmer. Java supports the usual logical conditions, you can use these conditions to perform different actions for different dicisions."}
