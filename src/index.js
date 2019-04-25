ad29db788be413a9cbfbb375fc1dfa28a81b9cf5
Christian Coffey <coffeychristian@gmail.com>
2019-04-24 20:26:30 -0400

    Update index.js

--- src/index.js
+++ src/index.js
@@ -34,7 +34,7 @@ ReactDOM.render(
             category="Welcome!"
             description={"First, we're going to learn about variables. Variables in code, like in math, can take on any value that you assign to them. However, unlike variables in math, Java variables need to be assigned a type. The type of the variable determines the value it can hold. So, for example, the type 'String' can hold text data, and the type 'int' can hold integer values. There are more types in Java, but we'll start by looking at an example of type String."}
             objectives={"Now, let's try to assign a variable name. Let's call the string variable var1 and assign the value of cat to it. Then let's call the int variable var2 and assign the number 3 to it."}
-            hints={["Consider following examples:", <li>Don't forget the quotation marks!!!!!!!!!!!!!!!!!</li>]}
+            hints={["Consider following examples:", <li>Don't forget the quotation marks!</li>]}
             codeBlock={["//Master JAVA Variables! \n\npublic static void main(String args[]){\n\n//Create a string variable\nstring hairColor =",<input type='text' id='bb'placeholder="e.g. red"/>,"\n\n}"]}/>} />
           <Route path="/Module2" render={(routeProps) => <Module {...routeProps} title="Basic Java: If statement"
             category="Welcome!"
